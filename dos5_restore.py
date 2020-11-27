2fda91579b684e99075bad07932b0c9d66176d25
jtbattle <frustum@pobox.com>
2020-11-27 17:52:37 -0600

    Get rid of non-ASCII chars from program
    When I cut & pasted the MIT license text, it brought in non-ASCII characters, which python complained about.
    
    There was also an extraneous print statement in one location that snuck in there somehow.

--- dos5_restore.py
+++ dos5_restore.py
@@ -32,10 +32,10 @@
 
 # License
 # -----------------------
-# Copyright © 2020 Jim Battle
-# 
+# Copyright (c) 2020 Jim Battle
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the “Software”), to deal
+# of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
@@ -44,7 +44,7 @@
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 # 
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -498,7 +498,6 @@ for action in actions:
         Path(head).mkdir(parents=True, exist_ok=True)
     # write the chunk to the target
     mode = 'wb' if (action.seq == 1) else 'ab'
-    print
     with open(action.dst_file, mode) as fh:
         fh.write(chunk)
     if args.timestamp:
